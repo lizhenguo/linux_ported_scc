72194a9d6a13510c37e4794347c27d8690e0244c
lizhenguo <zhenguoli@hust.edu.cn>
2015-12-31 09:53:03 +0800

    Just code according to the book is not a good idea, I should code on my own way, that is not just copyting the code on the book. Only after you know how the code works, implementing it in your own way.

--- scanner/dynstring.h
+++ scanner/dynstring.h
@@ -1,6 +1,11 @@
 typedef struct DynString {
     int len;    // string length
     int capacity;   // buffer length containing the string
-    char *ps;   // pointer to the string
+    char * data;   // pointer to the string
 } DynString; 
+
+void dynstring_reset(DynString *);
+void dynstring_chcat(DynString *, char);
+
+
 
